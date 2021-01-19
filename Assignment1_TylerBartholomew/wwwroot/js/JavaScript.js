452ed041677bf95b6b0e67577f7ce6009c0e027f
Tyler Bartholomew <tyler.bartholomew96@gmail.com>
2021-01-19 16:03:00 -0700

    recommit

--- Assignment1_TylerBartholomew/wwwroot/js/JavaScript.js
+++ Assignment1_TylerBartholomew/wwwroot/js/JavaScript.js
@@ -1,6 +1,15 @@
 ﻿var submit = document.getElementById("submitButton");
 
 submit.addEventListener("click", function () {
-    alert("hello world!")
 
-}
+
+    alert("Overall Grade: "
+        + parseInt(document.getElementById("assignments").value)*.5
+        + parseInt(document.getElementById("projects").value)*.1
+        + parseInt(document.getElementById("quizzes").value)*.1
+        + parseInt(document.getElementById("exams").value)*.2
+        + parseInt(document.getElementById("intex").value)*.1
+
+    )
+
+});
